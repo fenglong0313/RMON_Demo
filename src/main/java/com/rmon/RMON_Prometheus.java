c41c7b3d585f84bd25c82d82b4177ae45a9c99d4
fenglong <1074687505@qq.com>
2021-05-18 16:33:01 +0800

    Second_Git Git Commit

--- src/main/java/com/rmon/RMON_Prometheus.java
+++ src/main/java/com/rmon/RMON_Prometheus.java
@@ -6,6 +6,8 @@ public class RMON_Prometheus {
     public static void main(String[] args) {
         System.out.println(1);
         System.out.println(2);
+        System.out.println(3 + "Second_Git");
+        System.out.println(4 + "Second_Git");
 
 
 
